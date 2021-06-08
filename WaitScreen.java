1cf1a022cd88419d09208600eef416d8e803d7cc
Annie <annie.li.29122@gmail.com>
2021-06-08 11:00:35 -0400

    adding waiting screen

--- WaitScreen.java
+++ WaitScreen.java
@@ -24,19 +24,19 @@ import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
 
 // Create a class that extends BorderPane
-public class WelcomeScreen extends BorderPane {
+public class WaitScreen extends BorderPane {
 
     private MainApp mainApp;
     public static String file_name;
 
-    public WelcomeScreen(MainApp app) {
+    public WaitScreen(MainApp app) {
         super();
         this.mainApp = app;
 
         // Create a welcome label and format it
         Effect glow = new Glow(100.0);
         Label label = new Label();
-        label.setText(" Welcome to CS BS! ");
+        label.setText(" Waitng while others join... ");
         label.setEffect(glow);
         label.setTextFill(Color.web("#ff0c94"));
         label.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 48));
@@ -50,20 +50,6 @@ public class WelcomeScreen extends BorderPane {
         setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
         // format thigns vertically
         GridPane.setValignment(label, VPos.CENTER);
-
-        // Create an instructions button and format it
-        Button instructionButton = new Button("Instructions");
-        instructionButton.setTextFill(Color.web("#dcfc5c"));
-        instructionButton.setOnAction(e -> handleInstructionButton());
-        DropShadow dropshadow = new DropShadow();
-        dropshadow.setColor(Color.web("#dcfc5c"));
-        instructionButton.setEffect(dropshadow);
-        instructionButton.setStyle("-fx-background-color: #000000; ");
-        // instructionButton.setStyle("-fx-effect: dropshadow(three-pass-box,
-        // rgba(0,0,0,0.7), 5, 0.0, 0, 1)");
-        instructionButton.setStyle(
-                "-fx-font: 22 serif; -fx-background-color: #000000, linear-gradient(#000000 0%, #000000 20%, #000000 100%), linear-gradient(#000000, #000000), radial-gradient(center 50% 0%, radius 100%, #000000, #000000)");
-        // instructionButton.setFont(Font.loadFont("CaviarDreams.ttf", 22));
 
         // Create a play button and format it
         Button play = new Button("Start");
@@ -90,7 +76,7 @@ public class WelcomeScreen extends BorderPane {
         // Put these buttons into a GridPane at the bottom of the screen and set its
         // spacing
         GridPane bottom = new GridPane();
-        bottom.addRow(0, play, instructionButton);
+        bottom.addRow(0, play);
 
         bottom.setHgap(20);
         bottom.setVgap(10);
@@ -104,12 +90,9 @@ public class WelcomeScreen extends BorderPane {
     }
 
     // Define methods to handle each button
-    private void handleInstructionButton() {
-        mainApp.showInstructionScreen();
-    }
 
     public void handleButtonStart() {
-        mainApp.showWaitScreen();
+        mainApp.showGameScreen();
     }
 
     // public void handleButtonSettings() {
